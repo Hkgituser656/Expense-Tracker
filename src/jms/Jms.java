932aec80b1bae868ba1c2e78984124eeeec36017
Harsh Kumar <149146548+Hkgituser656@users.noreply.github.com>
2024-05-01 16:52:58 +0530

    Add files via upload

---
+++ src/jms/Jms.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jms;
+
+/**
+ *
+ * @author 91955
+ */
+public class Jms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        top t = new top();
+        t.setVisible(true);
+    
+    }
+    
+}
